0103ac80475a5c06267323dae9e5df59fd6e0d55
sseung95 <seung3657@gmail.com>
2024-12-06 18:06:10 +0900

    Feat: LoginForm 성공, 에러 로직 추가

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,25 +1,41 @@
-import Image from "next/image";
+'use client';
 
-import LoginForm from "@/components/auth/LoginForm/LoginForm";
+import Image from 'next/image';
+
+import LoginForm from '@/components/auth/LoginForm/LoginForm';
+import { useRouter } from 'next/navigation';
 
 export default function LoginPage() {
-	// TODO : 성공, 에러했을 때의 처리 필요
-	return (
-		<div className="flex flex-col flex-1 h-full w-full justify-center">
-			<div className="flex items-center justify-center w-full gap-[12rem]">
-				<div className="flex-1">
-					<Image
-						src="/images/sally.png"
-						alt="sally"
-						width={486}
-						height={584}
-						className="text-white"
-					/>
-				</div>
-				<div className="flex-1">
-					<LoginForm />
-				</div>
-			</div>
-		</div>
-	);
+  const router = useRouter();
+
+  const handleLoginSuccess = () => {
+    // 로그인 성공 시 메인 페이지로 이동
+    router.push('/');
+  };
+
+  const handleLoginError = (error: Error) => {
+    console.error('로그인 실패', error);
+  };
+
+  return (
+    <div className="flex flex-col flex-1 h-full w-full justify-center">
+      <div className="flex items-center justify-center w-full gap-[12rem]">
+        <div className="flex-1">
+          <Image
+            src="/images/sally.png"
+            alt="sally"
+            width={486}
+            height={584}
+            className="text-white"
+          />
+        </div>
+        <div className="flex-1">
+          <LoginForm
+            onSuccess={handleLoginSuccess}
+            onError={handleLoginError}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
